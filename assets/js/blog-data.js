723cba67223cfe6f3d026a67961742ecd44b69fb
Marc Ramos <m4rklar@gmail.com>
2026-06-01 22:44:37 +0200

    remove reveal image

--- assets/js/blog-data.js
+++ assets/js/blog-data.js
@@ -5,7 +5,7 @@ var articulosEssencia = [
     {
         "titulo": "Pròleg del joc",
         "fecha": "2026-06-01",
-        "url": "posts/essencia-06-2026-2.html",
+        "url": "posts/essencia-06-2026-1.html",
         "categoria": "Historia",
         "resumen": "Un petit esquirol s’enfila al damunt d'una pila de llibres. Va vestit amb una jaqueta negre sense mànigues i botonada. Amb aires d’urgència,  treu d’una butxaca unes ulleres de lents desproporcionalme..."
     }
